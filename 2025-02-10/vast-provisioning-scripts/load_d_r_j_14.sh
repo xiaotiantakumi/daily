89209f5ee58ec67ceb62ee5750d4afcc862570ff
Takumi Oda <xiaotiantakumi@gmail.com>
2025-02-11 11:24:19 +0900

    Modify Ollama model pull script: comment out workspace change and 1.5b model pull

--- 2025-02-10/vast-provisioning-scripts/load_d_r_j_14.sh
+++ 2025-02-10/vast-provisioning-scripts/load_d_r_j_14.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # Activate the main virtual environment
 . /venv/main/bin/activate
 
-cd "${WORKSPACE}/" 
+# cd "${WORKSPACE}/" 
 
 ollama pull hhao/qwen2.5-coder-tools:0.5b
-ollama pull hhao/qwen2.5-coder-tools:1.5b
+# ollama pull hhao/qwen2.5-coder-tools:1.5b
